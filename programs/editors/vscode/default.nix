fe077b3353156563a7d17850a9d3cd4be65f899d
huantian <davidtianli@gmail.com>
2022-07-28 14:46:26 -0700

    add some vscode extensions

--- programs/editors/vscode/default.nix
+++ programs/editors/vscode/default.nix
@@ -6,6 +6,20 @@
     userSettings = import ./settings.nix { inherit pkgs; };
     extensions = with pkgs.vscode-extensions; [
       matklad.rust-analyzer-nightly  # Provided by fenix
+
+      njpwerner.autodocstring
+      bungcip.better-toml
+      ms-dotnettools.csharp
+      vadimcn.vscode-lldb
+      adpyke.codesnap
+
+      redhat.java
+      oderwat.indent-rainbow
+      pkief.material-icon-theme
+
+      github.vscode-pull-request-github
+      eamodio.gitlens
+      redhat.vscode-yaml
     ];
   };
 }
--- programs/editors/vscode/settings.nix
+++ programs/editors/vscode/settings.nix
@@ -2,7 +2,7 @@
 
 {
     #############
-    # General ##
+    ## General ##
     #############
 
     # Terminal Setup
