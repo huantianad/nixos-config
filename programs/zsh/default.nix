86e4395f906f5ce6ba2835fabc6b4bb197176a6f
huantian <davidtianli@gmail.com>
2022-08-02 12:58:38 -0700

    zsh: add xdg home exports

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -63,6 +63,11 @@
 
         # Use bat for manpages
         export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+        export XDG_DATA_HOME="$HOME/.local/share"
+        export XDG_CONFIG_HOME="$HOME/.config"
+        export XDG_STATE_HOME="$HOME/.local/state"
+        export XDG_CACHE_HOME="$HOME/.cache"
       '';
     };
 
