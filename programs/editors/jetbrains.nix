36fe03129d076439a0d5d8fc0da998022163e2e4
huantian <davidtianli@gmail.com>
2022-08-16 13:55:18 -0700

    Rider now works! bundle it with msbuild

--- programs/editors/jetbrains.nix
+++ programs/editors/jetbrains.nix
@@ -1,10 +1,40 @@
 { config, pkgs, lib, inputs, ... }:
 
+let
+  dotnetCombined = with pkgs.dotnetCorePackages;
+    combinePackages [ sdk_6_0 ];
+  rider-fhs = pkgs.buildFHSUserEnv {
+    name = "rider-fhs";
+    runScript = "";
+    targetPkgs = pkgs: with pkgs; [
+      dotnetCombined
+      dotnetPackages.Nuget
+      mono
+      msbuild
+    ];
+  };
+  rider = pkgs.jetbrains.rider.overrideAttrs (attrs: {
+    postInstall = ''
+      mv $out/bin/rider $out/bin/.rider-unwrapped
+
+      cat >$out/bin/rider <<EOL
+      #!${pkgs.bash}/bin/bash
+      ${rider-fhs}/bin/rider-fhs $out/bin/.rider-unwrapped "\$@"
+      EOL
+
+      chmod +x $out/bin/rider
+    '' + attrs.postInstall or "";
+  });
+in
 {
   config = {
-    environment.systemPackages = with pkgs.jetbrains; [
+    environment.systemPackages = [
       rider
-      # clion
     ];
+    home-manager.users.huantian.home.file = {
+      ".local/share/applications/jetbrains-rider.desktop".text = ''
+        Exec="${rider}/bin/rider"
+      '';
+    };
   };
 }
