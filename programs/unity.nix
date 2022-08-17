0daa054fd41fac686f7a3216188c58e859cd68f5
huantian <davidtianli@gmail.com>
2022-08-16 19:18:03 -0700

    some unity package updates to make editor work-ish kinda

--- programs/unity.nix
+++ programs/unity.nix
@@ -11,13 +11,11 @@ let
     };
 
     nativeBuildInputs = with pkgs; [
-      autoPatchelfHook
-      wrapGAppsHook
       dpkg
       makeShellWrapper
     ];
 
-    buildInputs = with pkgs; [
+    extraLibs = with pkgs; with xorg; [
       glibc
       gcc-unwrapped
       cups
@@ -40,9 +38,12 @@ let
       nss_latest
       libdrm
       mesa
-    ];
+      nspr
+      atk
+      dbus
+      at-spi2-atk
+      gnome2.pango
 
-    libPath = with pkgs; lib.makeLibraryPath [
       libva
       openssl_3
       openssl
@@ -56,7 +57,44 @@ let
       wayland
       cpio
       icu
+
+      # editor
+      libglvnd #ligbl
+      xorg.libX11
+      xorg.libXcursor
+      glib
+      gdk-pixbuf
+      libxml2
+      zlib
+      clang
+
+      libsForQt5.full
+      xorg.libXinerama
+      hicolor-icon-theme
+      gtk4
+
+      # for me personally
+      harfbuzz
+      libogg
+
+      # bug reporter stuff???
+      fontconfig
+      freetype
+      lsb-release
     ];
+
+    fhsEnv = pkgs.buildFHSUserEnv {
+      name = "${name}-fhs-env";
+      targetPkgs = pkgs: unityhub.extraLibs;
+      runScript = "";
+      extraBuildCommands = ''
+        # mkdir -p $out/usr/share/glib-2.0
+        # ln -s ${pkgs.glib.getSchemaPath pkgs.gtk3} $out/usr/share/glib-2.0/schemas
+        mkdir -p $out/usr/lib/qt5/
+        ln -s ${pkgs.libsForQt5.full}/lib/qt-5.15.5/qml $out/usr/lib/qt5/qml
+        ln -s ${pkgs.libsForQt5.full}/lib/qt-5.15.5/plugins $out/usr/lib/qt5/plugins
+      '';
+    };
 
     sourceRoot = ".";
     unpackPhase = "dpkg -x $src .";
@@ -64,7 +102,7 @@ let
     dontConfigure = true;
     dontBuild = true;
 
-    installPhase= ''
+    installPhase = ''
       runHook preInstall
 
       mkdir -p $out/bin
@@ -73,9 +111,8 @@ let
       mv opt/ $out/opt
 
       # `unityhub` is a shell wrapper that runs `unityhub-bin`
-      # We don't need this so we can replace it with the binary directly.
+      # Which we don't need and replace with our own custom wrapper later
       rm $out/opt/unityhub/unityhub
-      mv $out/opt/unityhub/unityhub-bin $out/opt/unityhub/unityhub
 
       # Link binary
       ln -s $out/opt/unityhub/unityhub $out/bin/unityhub
@@ -84,8 +121,15 @@ let
       substituteInPlace $out/share/applications/unityhub.desktop \
         --replace /opt/unityhub/unityhub $out/opt/unityhub/unityhub
 
-      wrapProgramShell $out/opt/unityhub/unityhub \
-        --prefix LD_LIBRARY_PATH : ${libPath}
+      cat >$out/opt/unityhub/unityhub <<EOL
+      #!${pkgs.bash}/bin/bash
+      export LD_LIBRARY_PATH="\$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib:/usr/lib:/usr/lib32"
+      export GSETTINGS_SCHEMA_DIR=${pkgs.glib.getSchemaPath pkgs.gtk3}
+      export QML2_IMPORT_PATH="${pkgs.libsForQt5.full}/lib/qt-5.15.5/qml:\$QML2_IMPORT_PATH"
+      export QT_PLUGIN_PATH="${pkgs.libsForQt5.full}/lib/qt-5.15.5/plugins:\$QT_PLUGIN_PATH"
+      ${fhsEnv}/bin/${name}-fhs-env $out/opt/unityhub/unityhub-bin "\$@"
+      EOL
+      chmod +x $out/opt/unityhub/unityhub
 
       # From the .deb's postinst hook, not sure if necessary
       chmod 4755 '$out/opt/unityhub/chrome-sandbox' || true
@@ -99,13 +143,13 @@ let
       description = "Download and manage Unity Projects and installations.";
       homepage = "https://unity3d.com/";
       license = licenses.unfree;
-      maintainers = with maintainers; [ ];
+      maintainers = with maintainers; [ huantian ];
       platforms = [ "x86_64-linux" ];
     };
   };
 in
-  {
-    config = {
-      environment.systemPackages = [ unityhub ];
-    };
-  }
+{
+  config = {
+    environment.systemPackages = [ unityhub ];
+  };
+}
