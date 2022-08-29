b3331603b5f4a7ec74b9d1f14444af769b018cd8
huantian <davidtianli@gmail.com>
2022-08-28 17:33:22 -0700

    some xbindkeys

--- programs/xbindkeys.nix
+++ programs/xbindkeys.nix
@@ -4,17 +4,28 @@
   config = {
     environment.systemPackages = [ pkgs.xbindkeys ];
 
+    systemd.services."xbindkeys" = {
+      description = "xbindkeys";
+      partOf = [ "graphical-session.target" ];
+      wantedBy = [ "xsession.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.xbindkeys}/bin/xbindkeys --nodaemon -f /home/huantian/.xbindkeysrc";
+        User = "huantian";
+        Group = "users";
+      };
+    };
+
     home-manager.users.huantian.home.file = {
-      ".config/autostart/xbindkeys.desktop".text = ''
-        [Desktop Entry]
-        Encoding=UTF-8
-        Version=${pkgs.xbindkeys.version}
-        Name=xbindkeys
-        Comment=Start xbindkeys service.
-        Exec=${pkgs.xbindkeys}/bin/xbindkeys -f /home/huantian/.xbindkeysrc
-        StartupNotify=false
-        Terminal=false
-      '';
+      # ".config/autostart/xbindkeys.desktop".text = ''
+      #   [Desktop Entry]
+      #   Encoding=UTF-8
+      #   Version=${pkgs.xbindkeys.version}
+      #   Name=xbindkeys
+      #   Comment=Start xbindkeys service.
+      #   Exec=
+      #   StartupNotify=false
+      #   Terminal=false
+      # '';
 
       ".xbindkeysrc/".text = ''
         "${pkgs.playerctl}/bin/playerctl play-pause"
