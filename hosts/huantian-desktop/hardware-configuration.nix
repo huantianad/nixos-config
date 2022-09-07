8181bf41d2499aba807962431f009d662d77729a
huantian <davidtianli@gmail.com>
2022-09-07 16:50:36 -0700

    add some swap to file

--- hosts/huantian-desktop/hardware-configuration.nix
+++ hosts/huantian-desktop/hardware-configuration.nix
@@ -28,7 +28,10 @@
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    size = 4096;
+    device = "/swapfile";
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
