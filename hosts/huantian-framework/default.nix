7b11f1ab0a21aa9c77b28709da81bc54407b9550
huantian <davidtianli@gmail.com>
2022-09-22 12:40:06 -0700

    libinput-gestures/ydotool systemd service

--- hosts/huantian-framework/default.nix
+++ hosts/huantian-framework/default.nix
@@ -87,6 +87,18 @@
     checkra1n
   ];
 
+  home-manager.users.huantian.home.file =
+  let
+    ydotool-service = "${pkgs.ydotool}/share/systemd/user/ydotool.service";
+    gestures-service = "${pkgs.libinput-gestures}/share/systemd/user/libinput-gestures.service";
+  in
+  {
+    ".config/systemd/user/ydotool.service".source = ydotool-service;
+    ".config/systemd/user/default.target.wants/ydotool.service".source = ydotool-service;
+    ".config/systemd/user/libinput-gestures.service".source = gestures-service;
+    ".config/systemd/user/graphical-session.target.wants/libinput-gestures.service".source = gestures-service;
+  };
+
   services.xserver.libinput.touchpad = {
     naturalScrolling = true;
   };
