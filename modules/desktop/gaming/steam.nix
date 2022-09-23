4d000252762368220e73d3d07500174b2c5c53c9
huantian <davidtianli@gmail.com>
2022-09-22 17:33:16 -0700

    steamcmd

--- modules/desktop/gaming/steam.nix
+++ modules/desktop/gaming/steam.nix
@@ -12,6 +12,10 @@ in
   config = mkIf cfg.enable {
     programs.steam.enable = true;
 
+    environment.systemPackages = with pkgs; [
+      steamPackages.steamcmd
+    ];
+
     # better for steam proton games
     systemd.extraConfig = "DefaultLimitNOFILE=1048576";
 
