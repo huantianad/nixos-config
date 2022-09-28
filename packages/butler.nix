0c01e7a1958b51131569d43df92898ae60c9c8ea
huantian <davidtianli@gmail.com>
2022-09-27 19:01:58 -0700

    some butler stuff

--- packages/butler.nix
+++ packages/butler.nix
@@ -11,8 +11,20 @@ buildGoModule rec {
     sha256 = "sha256-vciSmXR3wI3KcnC+Uz36AgI/WUfztA05MJv1InuOjJM=";
   };
 
-  # nix-prefetch '{ sha256 }: (callPackage (import ./asdf) { }).go-modules.overrideAttrs (_: { modSha256 = sha256; })'
+  # nix-prefetch '{ sha256 }: (callPackage (import ./butler.nix) { }).
+  # go-modules.overrideAttrs (_: { modSha256 = sha256; })'
   vendorSha256 = "sha256-vdMu4Q/1Ami60JPPrdq5oFPc6TjmL9klZ6W+gBvfkx0=";
+
+  ldflags =
+    let bi = "github.com/itchio/butler/buildinfo";
+    in
+    [
+      "-X ${bi}.Version=v${version}"
+      "-X ${bi}.BuiltAt=$SOURCE_DATE_EPOCH"
+      # "-X ${bi}.Commit=${process.env.CI_BUILD_REF || ""}"
+      "-w"
+      "-s"
+    ];
 
   buildInputs = [
     brotli
@@ -20,8 +32,10 @@ buildGoModule rec {
   ];
 
   preCheck = ''
-    # Skip tests that require a path to Butler
+    # Skips tests that require a path to built Butler binary
     export CI=1
+    # export GOFLAGS="$GOFLAGS --butlerPath='$out/bin/butler'"
+    # go test -v ./butlerd/integrate --butlerPath='\$GOPATH/bin/butler'
   '';
 
   meta = with lib; {
