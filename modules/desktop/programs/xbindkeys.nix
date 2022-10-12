f8bb9d9239c31c8c2eaa518de64e3672b8ff3a1e
huantian <davidtianli@gmail.com>
2022-10-12 13:18:02 -0700

    remove xbindkeys service

--- modules/desktop/programs/xbindkeys.nix
+++ modules/desktop/programs/xbindkeys.nix
@@ -12,18 +12,6 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = [ pkgs.xbindkeys ];
 
-    systemd.services."xbindkeys" = {
-      description = "xbindkeys";
-      partOf = [ "graphical-session.target" ];
-      wantedBy = [ "xsession.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.xbindkeys}/bin/xbindkeys --nodaemon -f /home/huantian/.xbindkeysrc";
-        KillMode = "process";
-        User = "huantian";
-        Group = "users";
-      };
-    };
-
     home-manager.users.huantian.home.file = {
       ".xbindkeysrc/".text = ''
         "${pkgs.playerctl}/bin/playerctl play-pause"
