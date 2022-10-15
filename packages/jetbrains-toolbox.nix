8ebcafd209badfa869c014200eb52f13eb41742b
huantian <davidtianli@gmail.com>
2022-10-15 12:31:09 -0700

    try to update jetbrains toolbox

--- packages/jetbrains-toolbox.nix
+++ packages/jetbrains-toolbox.nix
@@ -2,16 +2,16 @@
 
 appimageTools.wrapAppImage rec {
   name = "jetbrains-toolbox";
-  version = "1.25.12627";
+  version = "1.26.2.13244";
 
   src =
     let
       tarball = fetchTarball {
         url = "https://download.jetbrains.com/toolbox/jetbrains-toolbox-${version}.tar.gz";
-        sha256 = "1k9lnk49ns9494pj4xm7jzhkw93khydx1a5azwr7pp2zbkxifi80";
+        sha256 = "sha256:172cp5lmpkr5jqqpqy3qnpm0vlw8ni5kkh3a211nma89903x6c5h";
       };
     in
-    runCommand "jetbrains-toolbox-unextracted"
+    runCommand "jetbrains-toolbox-extracted"
       { buildInputs = [ appimageTools.appimage-exec ]; } ''
       appimage-exec.sh -x $out ${tarball}/jetbrains-toolbox
     '';
