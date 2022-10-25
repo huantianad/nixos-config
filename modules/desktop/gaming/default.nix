ddb2f7c81ff2bf1624a58eb1f744e992ee55719a
huantian <davidtianli@gmail.com>
2022-10-24 21:22:57 -0700

    tetrio plus

--- modules/desktop/gaming/default.nix
+++ modules/desktop/gaming/default.nix
@@ -16,7 +16,19 @@ in
       scarab
       cockatrice
       airshipper
-      tetrio-desktop
+      (tetrio-desktop.overrideAttrs (oldAttrs:
+        let
+          tetrio-plus = fetchzip {
+            url = "https://gitlab.com/UniQMG/tetrio-plus/uploads/a9647feffc484304ee49c4d3fd4ce718/tetrio-plus_0.23.13_app.asar.zip";
+            sha256 = "sha256-NSOVZjm4hDXH3f0gFG8ijLmdUTyMRFYGhxpwysoYIVg=";
+          };
+        in
+        {
+          installPhase = oldAttrs.installPhase + "
+            cp ${tetrio-plus}/app.asar $out/opt/TETR.IO/resources/
+          ";
+        }
+      ))
     ];
   };
 }
