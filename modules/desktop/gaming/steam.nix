842aff61868d6680177ffda00dd46c6a1b056b97
huantian <davidtianli@gmail.com>
2022-11-16 18:37:41 -0700

    gsettings workaround is no logner needed for steam

--- modules/desktop/gaming/steam.nix
+++ modules/desktop/gaming/steam.nix
@@ -19,20 +19,13 @@ in
     # better for steam proton games
     systemd.extraConfig = "DefaultLimitNOFILE=1048576";
 
-    nixpkgs.overlays = [
-      (self: super: {
-        steam = super.steam.override {
-          extraPkgs = pkgs: [
-            # I think I added these libraries for Demoncrawl
-            pkgs.libssh
-            pkgs.brotli
-          ];
-          extraProfile = ''
-            # Fix "No GSettings schemas are installed on the system"
-            export GSETTINGS_SCHEMA_DIR=${pkgs.glib.getSchemaPath pkgs.gtk3}
-          '';
-        };
-      })
-    ];
+    nixpkgs.config.packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          libssh
+          brotli
+        ];
+      };
+    };
   };
 }
