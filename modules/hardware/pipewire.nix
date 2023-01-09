8c6832cbef9820b7c5513d8f01ce5c8b075d7b9c
huantian <davidtianli@gmail.com>
2023-01-09 15:03:41 -0700

    user: add to power group, fix pipewire
    audio group hopefully to fix mic issues on wayland framework

--- modules/hardware/pipewire.nix
+++ modules/hardware/pipewire.nix
@@ -11,8 +11,6 @@ in
 
   config = mkIf cfg.enable {
     # Enable sound with pipewire.
-    sound.enable = true;
-    hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
--- modules/user.nix
+++ modules/user.nix
@@ -5,7 +5,7 @@
     users.users.huantian = {
       isNormalUser = true;
       description = "David Li";
-      extraGroups = [ "networkmanager" "wheel" "input" ];
+      extraGroups = [ "networkmanager" "wheel" "input" "power" ];
       shell = pkgs.zsh;
       hashedPassword = "$6$.r68MxqkJ5EkYsO1$UcxXYU5ZS7mKLw74SqHzoLYbc6sJzrzCmTlY7oyC0aPBveceIFc0RpEMhHyTGmtZ1ROmH0mMyCJw8XyEoyYvr1";
     };
