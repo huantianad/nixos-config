198c141b43dd8ef803c784239f32202b1619fbf5
huantian <davidtianli@gmail.com>
2023-01-24 21:09:24 -0700

    framework: thermald, redistributable firmware

--- hosts/huantian-framework/default.nix
+++ hosts/huantian-framework/default.nix
@@ -83,6 +83,8 @@
   # Disable power-profiles-daemon as it conflicts with tlp
   services.power-profiles-daemon.enable = false;
 
+  services.thermald.enable = true;
+
   # Disable fingerprint auth on first login
   # SDDM fingerprint is buggy and I need to type password for KWallet anyway
   security.pam.services.sddm.fprintAuth = false;
--- hosts/huantian-framework/hardware-configuration.nix
+++ hosts/huantian-framework/hardware-configuration.nix
@@ -43,4 +43,7 @@
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Added this myself to enable the above code
+  hardware.enableRedistributableFirmware = true;
 }
