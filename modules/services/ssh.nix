ad5ce2b9ff608ee864f75ad31eee6edab81ca011
huantian <davidtianli@gmail.com>
2023-02-10 16:26:13 -0700

    ssh: Switch to unified ed ssh key

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -18,11 +18,8 @@ in
       };
     };
 
-    users.users.root.openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDULu9ey1UPD0z2DFD9NnZzPZkxsows/jD8UkWgzYZZpo3gMFCfMjdAHdMYBFx760uzSmgIQ9LYuw+pimnoSwOIdPyQpkVyK0AV0iCj5gpsm6dmsPTt0G/+rUdka2Wf5a876LSsU5FBo/Ei+d2cCPSOW8Nn9ZYH2ZtbiSE1BCx2nns+laFdOvOlV/Ff6jzg31nRsFFui/MlmhCyxy2grEeCJtlw9Ykg1OBLRETE2KEuqnOUbDwVPaL3ZAlV8SPQX6v5YYPY8ZLzVUyKWJF0BJrVKwMMzkarnt1tp9S+2SYSaszo5C4ljXAfanA7WcIYbg+8kKOcWymzUkE5/6AbPuAn ssh-key-2022-03-04"
-    ];
     users.users.huantian.openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDULu9ey1UPD0z2DFD9NnZzPZkxsows/jD8UkWgzYZZpo3gMFCfMjdAHdMYBFx760uzSmgIQ9LYuw+pimnoSwOIdPyQpkVyK0AV0iCj5gpsm6dmsPTt0G/+rUdka2Wf5a876LSsU5FBo/Ei+d2cCPSOW8Nn9ZYH2ZtbiSE1BCx2nns+laFdOvOlV/Ff6jzg31nRsFFui/MlmhCyxy2grEeCJtlw9Ykg1OBLRETE2KEuqnOUbDwVPaL3ZAlV8SPQX6v5YYPY8ZLzVUyKWJF0BJrVKwMMzkarnt1tp9S+2SYSaszo5C4ljXAfanA7WcIYbg+8kKOcWymzUkE5/6AbPuAn ssh-key-2022-03-04"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdHdFlnoznfUXyvZ5m79WkI5xGQMO1U7M8N6t6KrBA3 huantian@huantian-desktop"
     ];
   };
 }
