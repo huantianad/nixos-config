ce746d0de5c6dc64f498c4b76f4a40ced82dd6e7
huantian <davidtianli@gmail.com>
2023-03-12 17:26:25 -0700

    nvidia hardware acc

--- modules/dev/cc.nix
+++ modules/dev/cc.nix
@@ -12,7 +12,6 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       clang
-      gcc
       bear
       gdb
       cmake
--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -12,12 +12,26 @@ in
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "nvidia" ];
 
+    environment.variables.MOZ_DISABLE_RDD_SANDBOX = "1";
+    environment.variables.LIBVA_DRIVER_NAME = "nvidia";
+
     hardware.opengl = {
       enable = true;
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
-        libvdpau-va-gl
+        (nvidia-vaapi-driver.overrideAttrs (attrs: {
+          version = "0.0.9";
+          src = fetchFromGitHub {
+            owner = "elFarto";
+            repo = attrs.pname;
+            rev = "v0.0.9";
+            sha256 = "sha256-mQtprgm6QonYiMUPPIcCbWxPQ/b2XuQiOkROZNPYaQk=";
+          };
+          buildInputs = attrs.buildInputs ++ [
+            pkgs.libdrm
+          ];
+        }))
       ];
     };
   };
