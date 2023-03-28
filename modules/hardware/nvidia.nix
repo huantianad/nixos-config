f6312fe7dddb30d521f3f2f05396f3f7c68bbeb5
huantian <davidtianli@gmail.com>
2023-03-28 13:46:30 -0700

    use nixpkgs updated nvidia-vaapi-driver

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -20,18 +20,7 @@ in
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
-        (nvidia-vaapi-driver.overrideAttrs (attrs: {
-          version = "0.0.9";
-          src = fetchFromGitHub {
-            owner = "elFarto";
-            repo = attrs.pname;
-            rev = "v0.0.9";
-            sha256 = "sha256-mQtprgm6QonYiMUPPIcCbWxPQ/b2XuQiOkROZNPYaQk=";
-          };
-          buildInputs = attrs.buildInputs ++ [
-            pkgs.libdrm
-          ];
-        }))
+        nvidia-vaapi-driver
       ];
     };
   };
