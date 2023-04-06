23078b661c0e0175f5eadf1bc7f9405f7b67a993
huantian <davidtianli@gmail.com>
2023-04-06 14:28:04 -0700

    aw: temp systemd unit

--- modules/desktop/programs/aw.nix
+++ modules/desktop/programs/aw.nix
@@ -18,5 +18,20 @@ in
       aw-watcher-window
       inputs.aw-watcher-custom.packages.${pkgs.system}.default
     ];
+
+    systemd.user.services.activitywatch = with inputs.aw.legacyPackages.${pkgs.system}; {
+      # wantedBy = [ "graphical-session.target" ];
+      path = [
+        aw-server-rust
+        aw-watcher-afk
+        aw-watcher-window
+        inputs.aw-watcher-custom.packages.${pkgs.system}.default
+      ];
+
+      serviceConfig = {
+        ExecStart = "${aw-qt}/bin/aw-qt";
+        Restart = "on-failure";
+      };
+    };
   };
 }
