d626d8c137088b3a19a73ecdbb442e6fd69209c0
huantian <davidtianli@gmail.com>
2023-04-25 16:23:53 -0700

    use basic activitywatch package

--- modules/desktop/programs/aw.nix
+++ modules/desktop/programs/aw.nix
@@ -11,27 +11,9 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with inputs.aw.legacyPackages.${pkgs.system}; [
-      aw-qt
-      aw-server-rust
-      aw-watcher-afk
-      aw-watcher-window
+    environment.systemPackages = [
+      inputs.aw.legacyPackages.${pkgs.system}.activitywatch
       inputs.aw-watcher-custom.packages.${pkgs.system}.default
     ];
-
-    systemd.user.services.activitywatch = with inputs.aw.legacyPackages.${pkgs.system}; {
-      # wantedBy = [ "graphical-session.target" ];
-      path = [
-        aw-server-rust
-        aw-watcher-afk
-        aw-watcher-window
-        inputs.aw-watcher-custom.packages.${pkgs.system}.default
-      ];
-
-      serviceConfig = {
-        ExecStart = "${aw-qt}/bin/aw-qt";
-        Restart = "on-failure";
-      };
-    };
   };
 }
