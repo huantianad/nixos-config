fe4b366affe54a604ab413b5ab749e09d06f05f6
huantian <davidtianli@gmail.com>
2023-04-30 17:13:33 -0700

    nix use unstable version

--- default.nix
+++ default.nix
@@ -16,6 +16,8 @@ with lib.my;
   # Configure nix and nixpkgs
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
   nix = {
+    package = pkgs.nixVersions.unstable;
+
     extraOptions = "experimental-features = nix-command flakes";
 
     settings = {
