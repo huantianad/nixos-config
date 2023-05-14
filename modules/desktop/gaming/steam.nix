8359254709da169df5df611ba904c217bd39e570
huantian <davidtianli@gmail.com>
2023-05-14 14:38:10 -0700

    steam: remove gtk workaround

--- modules/desktop/gaming/steam.nix
+++ modules/desktop/gaming/steam.nix
@@ -18,15 +18,5 @@ in
 
     # better for steam proton games
     systemd.extraConfig = "DefaultLimitNOFILE=1048576";
-
-    programs.steam.package = pkgs.steam.override {
-      # Workaround for steam beta GTK file picker issues, until #230375 is merged
-      extraProfile = "XDG_DATA_DIRS=\"\$XDG_DATA_DIRS:/usr/share/\"";
-
-      extraPkgs = pkgs: with pkgs; [
-        gsettings-desktop-schemas
-        hicolor-icon-theme
-      ];
-    };
   };
 }
