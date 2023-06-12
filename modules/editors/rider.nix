893f48a32df60d0c3ed94d4ea34cf1aa6df5dc62
huantian <davidtianli@gmail.com>
2023-06-11 21:42:18 -0700

    rider: add some more deps, change up postInstall

--- modules/editors/rider.nix
+++ modules/editors/rider.nix
@@ -8,13 +8,19 @@ let
   rider-fhs = pkgs.buildFHSUserEnv {
     name = "rider-fhs";
     runScript = "";
+
     targetPkgs = pkgs: with pkgs; [
       dotnetCorePackages.sdk_6_0
       dotnetPackages.Nuget
       mono
       msbuild
+
       # Personal development stuff
       xorg.libX11
+
+      # Rider Unity debugging
+      xorg.libXcursor
+      libglvnd
     ];
   };
 
@@ -22,23 +28,23 @@ let
     postInstall = ''
       # wrap rider in my custom fhs which has some dependencies
       mv $out/bin/rider $out/bin/.rider-unwrapped
-
-      cat >$out/bin/rider <<EOL
-      #!${pkgs.bash}/bin/bash
-      ${rider-fhs}/bin/rider-fhs $out/bin/.rider-unwrapped "\$@"
-      EOL
-
-      chmod +x $out/bin/rider
+      makeWrapper ${rider-fhs}/bin/rider-fhs $out/bin/rider \
+        --argv0 rider \
+        --add-flags $out/bin/.rider-unwrapped
 
       ## Making Unity Rider plugin work!
       # unity plugins looks for a build.txt at ../../build.txt, relative to binary
       # same for the product-info.json, both are used for BuildVersion and Numbers
-      ln -s $out/rider/build.txt $out/
-      ln -s $out/rider/product-info.json $out/
+      #ln -s $out/rider/build.txt $out/
+      #ln -s $out/rider/product-info.json $out/
 
       # looks for ../../plugins/rider-unity, relative to binary
       # it needs some dll file in there, which it uses to bind to rider
-      ln -s $out/rider/plugins $out/plugins
+      #ln -s $out/rider/plugins $out/plugins
+
+      shopt -s extglob
+      ln -s $out/rider/!(bin) $out/
+      shopt -u extglob
     '' + attrs.postInstall or "";
   });
 in
