b44a0666c95b3d9bb010d711f8781c766161d865
huantian <davidtianli@gmail.com>
2023-08-25 23:06:01 -0700

    packages/default.nix: remove deleted packages

--- packages/default.nix
+++ packages/default.nix
@@ -2,29 +2,13 @@
 
 let callPackage = pkgs.callPackage;
 in {
-  archipelago = callPackage ./archipelago { };
-
-  breath-theme = pkgs.libsForQt5.callPackage ./breath-theme { };
-
-  discord-screenaudio = pkgs.libsForQt5.callPackage ./discord-screenaudio { };
-
-  flatpak-xdg-utils = callPackage ./flatpak-xdg-utils { };
-
-  jetbrains-toolbox = callPackage ./jetbrains-toolbox { };
-
   musescore3 = pkgs.libsForQt5.callPackage ./musescore3 { };
 
-  r2modman = callPackage ./r2modman { };
-
   tjaplayer3-f = callPackage ./tjaplayer3-f { };
-
-  unityhub = callPackage ./unityhub { };
 
   unityhub-native = callPackage ./unityhub-native { };
 
   unnamed-sdvx-clone = callPackage ./unnamed-sdvx-clone { };
-
-  webcord = callPackage ./webcord { };
 
   xmage = callPackage ./xmage { };
 }
