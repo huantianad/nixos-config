a5409655ac04c130bc5bcd8ae9dab70c2e96f4ed
huantian <davidtianli@gmail.com>
2023-09-10 21:28:13 -0700

    framework: more battery settings

--- hosts/unstable/x86_64-linux/huantian-framework/default.nix
+++ hosts/unstable/x86_64-linux/huantian-framework/default.nix
@@ -75,6 +75,24 @@
   services.tlp.enable = true;
   services.tlp.settings = {
     PCIE_ASPM_ON_BAT = "powersupersave";
+
+    INTEL_GPU_MIN_FREQ_ON_BAT = 100;
+    INTEL_GPU_MAX_FREQ_ON_BAT = 800;
+    INTEL_GPU_BOOST_FREQ_ON_BAT = 1000;
+    INTEL_GPU_MIN_FREQ_ON_AC = 100;
+    INTEL_GPU_MAX_FREQ_ON_AC = 1300;
+    INTEL_GPU_BOOST_FREQ_ON_AC = 1300;
+
+    CPU_MIN_PERF_ON_AC = 0;
+    CPU_MAX_PERF_ON_AC = 100;
+    CPU_MIN_PERF_ON_BAT = 0;
+    CPU_MAX_PERF_ON_BAT = 30;
+
+    CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
   };
   # Disable power-profiles-daemon as it conflicts with tlp
   services.power-profiles-daemon.enable = false;
