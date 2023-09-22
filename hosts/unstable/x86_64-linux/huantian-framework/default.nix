dc1413fe531fa97784ad4c3a8c883abacf2792df
huantian <davidtianli@gmail.com>
2023-09-21 19:24:14 -0700

    trying this

--- hosts/unstable/x86_64-linux/huantian-framework/default.nix
+++ hosts/unstable/x86_64-linux/huantian-framework/default.nix
@@ -105,7 +105,7 @@
     CPU_SCALING_GOVERNOR_ON_AC = "powersave";
     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
   };
   # Disable power-profiles-daemon as it conflicts with tlp
