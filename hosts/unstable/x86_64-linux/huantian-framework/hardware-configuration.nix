68908264372ab2a30562fddc893193998a1cf1fc
huantian <davidtianli@gmail.com>
2023-09-22 22:43:59 -0700

    fix hibernate

--- hosts/unstable/x86_64-linux/huantian-framework/hardware-configuration.nix
+++ hosts/unstable/x86_64-linux/huantian-framework/hardware-configuration.nix
@@ -43,6 +43,7 @@
   };
 
   swapDevices = [{ label = "swap"; }];
+  boot.resumeDevice = "/dev/disk/by-label/swap";
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- modules/hardware/lanzaboote.nix
+++ modules/hardware/lanzaboote.nix
@@ -29,7 +29,6 @@ in
 
     environment.systemPackages = with pkgs; [
       sbctl
-      #
     ];
   };
 }
