550dd6b302e2429d4bd8635e3370bc9711a0996b
huantian <davidtianli@gmail.com>
2023-12-13 14:13:55 -0700

    nixos-23.05 -> nixos-23.11

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager-stable.url = "github:nix-community/home-manager/release-23.05";
+    home-manager-stable.url = "github:nix-community/home-manager/release-23.11";
     home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     aw-watcher-custom.url = "github:huantianad/aw-watcher-custom";
