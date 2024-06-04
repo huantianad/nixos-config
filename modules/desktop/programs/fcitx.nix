305b6592dbfa02fe6345cc019adc2227ada4ae21
huantian <davidtianli@gmail.com>
2024-06-03 19:42:42 -0700

    fcitx: remove vesktop patch, upstreamed

--- modules/desktop/programs/fcitx.nix
+++ modules/desktop/programs/fcitx.nix
@@ -17,24 +17,5 @@ in
         waylandFrontend = config.modules.desktop.wayland.enable;
       };
     };
-
-    nixpkgs.overlays = mkIf config.modules.desktop.wayland.enable [
-      (self: super: {
-        # We start fcitx with kde so we don't need the autostart file.
-        fcitx5-with-addons = super.fcitx5-with-addons.overrideAttrs
-          (attrs: {
-            postBuild = attrs.postBuild + "\nunlink $out/$autostart";
-          });
-        # Add the wayland ime flag to vesktop
-        # TODO: Remove this when we have a better mechanism for this!
-        vesktop = super.vesktop.overrideAttrs
-          (attrs: {
-            installPhase = replaceStrings
-              [ "WaylandWindowDecorations" ]
-              [ "WaylandWindowDecorations --enable-wayland-ime" ]
-              attrs.installPhase;
-          });
-      })
-    ];
   };
 }
