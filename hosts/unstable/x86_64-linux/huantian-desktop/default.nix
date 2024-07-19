9ca6b0f0693caa233236154945c73fb973d22788
huantian <davidtianli@gmail.com>
2024-07-19 13:58:35 -0700

    huantian-desktop: simplify a bunch of stuff

--- hosts/unstable/x86_64-linux/huantian-desktop/default.nix
+++ hosts/unstable/x86_64-linux/huantian-desktop/default.nix
@@ -81,40 +81,12 @@
     };
   };
 
-  # Disable virtualbox for now, takes a while to compile and I'm not using it
-  # virtualisation.virtualbox.host.enable = true;
-  # virtualisation.virtualbox.host.enableExtensionPack = true;
-
-  programs.dconf.enable = true;
-
   environment.systemPackages = with pkgs; [
   ];
-
-  # Causes error on rebuild sometime, slows startup => disable
-  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Increase size of /run/user/1000
   services.logind.extraConfig = "RuntimeDirectorySize=4G";
 
-  # Hardcode Nvidia X Server Settings changes to xorg config
-  # Fixes issues with second monitor being disabled on startup.
-  # services.xserver.screenSection = "Option \"metamodes\" \"DP-4: 2560x1440_155 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, HDMI-0: nvidia-auto-select +2560+360 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}\"";
-
-  # Fix high refresh rate on KDE https://bugs.kde.org/show_bug.cgi?id=433094#c15
-  # Also turn off flipping in nvidia-settings
-  # and run nvidia-settings --load-config-only on startup
-  hardware.nvidia.forceFullCompositionPipeline = true;
-  # environment.sessionVariables = {
-  #   KWIN_X11_REFRESH_RATE = "155000";
-  #   KWIN_X11_NO_SYNC_TO_VBLANK = "1";
-  # };
-
   # Earlyoom because unity + rider often locks up system with too much memory usage
   services.earlyoom.enable = true;
-
-  # VLC
-  networking.firewall.allowedTCPPorts = [8080];
-
-  # Factorio
-  networking.firewall.allowedUDPPorts = [34197];
 }
