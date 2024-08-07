3e4a4d28653a3a853e29ac0b78e99530edde01df
huantian <davidtianli@gmail.com>
2024-08-07 12:32:48 -0700

    huantian-server: don't sleep ever

--- hosts/stable/x86_64-linux/huantian-server/default.nix
+++ hosts/stable/x86_64-linux/huantian-server/default.nix
@@ -29,4 +29,8 @@
   };
 
   services.logind.lidSwitch = "ignore";
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
