8501b77470ca30e486c5b4969a3d7874eb4d8a89
huantian <davidtianli@gmail.com>
2024-08-26 22:20:51 -0700

    use nvidia beta by default

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -17,14 +17,9 @@ in {
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = ["nvidia"];
 
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
     hardware.nvidia.modesetting.enable = true;
     hardware.nvidia.open = true;
-
-    specialisation.nvidiaBeta.configuration = {
-      hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
-      boot.loader.systemd-boot.sortKey = "nvidiaBeta";
-    };
 
     environment.variables.MOZ_DISABLE_RDD_SANDBOX = "1";
     environment.variables.LIBVA_DRIVER_NAME = "nvidia";
