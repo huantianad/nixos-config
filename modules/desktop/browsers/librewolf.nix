365d7ba47d85b0d19b5b970ae556995749af265e
huantian <davidtianli@gmail.com>
2024-09-23 19:30:50 -0700

    librewolf: disable webgl

--- modules/desktop/browsers/librewolf.nix
+++ modules/desktop/browsers/librewolf.nix
@@ -16,25 +16,17 @@ in {
 
   config = mkIf cfg.enable {
     environment.systemPackages = let
-      withPlasma = pkgs.librewolf.override {nativeMessagingHosts = [pkgs.kdePackages.plasma-browser-integration];};
+      withPlasma = pkgs.librewolf.override {
+        nativeMessagingHosts =
+          lib.optional config.modules.desktop.kde.enable pkgs.kdePackages.plasma-browser-integration;
+      };
     in [withPlasma];
 
     home-manager.users.huantian.home.file = {
       ".librewolf/librewolf.overrides.cfg".text = ''
-        defaultPref("webgl.dxgl.enabled", true);
-        defaultPref("webgl.disabled", false);
-
-        defaultPref("webgl.enable-webgl2", true);
-        defaultPref("webgl.min_capability_mode", false);
-        defaultPref("webgl.disable-extensions", false);
-        defaultPref("webgl.disable-fail-if-major-performance-caveat", true);
-        defaultPref("webgl.enable-debug-renderer-info", true);
-
-        defaultPref("pdfjs.enableWebGL", true);
-
         defaultPref("dom.event.clipboardevents.enabled", true);
 
-        defaultPref("dom.webaudio.enabled", true);
+        // defaultPref("dom.webaudio.enabled", true);
 
         defaultPref("identity.fxaccounts.enabled", true);
         defaultPref("services.sync.engine.passwords", false);
