b31dffe3ad61a3d7df5a81a62a8415e553a9d0ff
huantian <davidtianli@gmail.com>
2024-12-04 12:03:12 -0800

    framework: remove podman

--- hosts/unstable/x86_64-linux/huantian-framework/default.nix
+++ hosts/unstable/x86_64-linux/huantian-framework/default.nix
@@ -94,16 +94,4 @@
 
   environment.systemPackages = with pkgs; [
   ];
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
 }
