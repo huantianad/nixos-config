15ba2def013f3999733a5244e0ce625c65843f98
huantian <davidtianli@gmail.com>
2025-01-18 12:30:28 -0800

    framework: use zram

--- hosts/unstable/x86_64-linux/huantian-framework/hardware-configuration.nix
+++ hosts/unstable/x86_64-linux/huantian-framework/hardware-configuration.nix
@@ -46,8 +46,7 @@
     fsType = "ext4";
   };
 
-  swapDevices = [{label = "swap";}];
-  boot.resumeDevice = "/dev/disk/by-label/swap";
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
