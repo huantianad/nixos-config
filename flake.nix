ecc17d63b333e977c248f93f8f6b0fbbbabbe0e8
huantian <davidtianli@gmail.com>
2025-05-27 11:05:53 -0700

    nixpkgs: 24.11 -> 25.05
    Flake lock file updates:
    
    • Updated input 'home-manager-stable':
        'github:nix-community/home-manager/d5f1f641b289553927b3801580598d200a501863?narHash=sha256-ypL9WAZfmJr5V70jEVzqGjjQzF0uCkz%2BAFQF7n9NmNc%3D' (2025-05-19)
      → 'github:nix-community/home-manager/83665c39fa688bd6a1f7c43cf7997a70f6a109f9?narHash=sha256-GaBRgxjWO1bAQa8P2%2BFDxG4ANBVhjnSjBms096qQdxo%3D' (2025-05-26)
    • Updated input 'nix-vscode-extensions':
        'github:nix-community/nix-vscode-extensions/cde730cb9dc2759104d7c8c27cb36d73189be1e2?narHash=sha256-Lvl26mCEatC5akRHywuXkYM6ZKc7R5T9m6u7rBPtNQw%3D' (2025-05-26)
      → 'github:nix-community/nix-vscode-extensions/0e0e608563707c9ee3919b3d1cab83beef984ad7?narHash=sha256-Xw7OtjL2y7m9rjbYAX1xluevyWY%2BfvjdFFM4MfiVk5Y%3D' (2025-05-27)
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/f09dede81861f3a83f7f06641ead34f02f37597f?narHash=sha256-92vihpZr6dwEMV6g98M5kHZIttrWahb9iRPBm1atcPk%3D' (2025-05-23)
      → 'github:nixos/nixpkgs/7c43f080a7f28b2774f3b3f43234ca11661bf334?narHash=sha256-rqc2RKYTxP3tbjA%2BPB3VMRQNnjesrT0pEofXQTrMsS8%3D' (2025-05-25)

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     nixos-hardware.url = "github:huantianad/nixos-hardware/master";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager-stable.url = "github:nix-community/home-manager/release-24.11";
+    home-manager-stable.url = "github:nix-community/home-manager/release-25.05";
     home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     aw-watcher-custom.url = "github:huantianad/aw-watcher-custom";
@@ -75,7 +75,9 @@
             home-manager = inputs.home-manager-stable;
           };
         }
-        .${nixpkgsType};
+        .${
+          nixpkgsType
+        };
 
       thisPkgs = mkPkgs table.nixpkgs system;
       thisLib = table.nixpkgs.lib.extend (self: super: {
