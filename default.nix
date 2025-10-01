42846f39866bcea27abc1c6382d34b184cf99043
huantian <davidtianli@gmail.com>
2025-10-01 10:16:50 -0700

    default.nix: remove unused cude substituters

--- default.nix
+++ default.nix
@@ -34,11 +34,9 @@ with lib.my; {
     settings = {
       substituters = [
         "https://nix-community.cachix.org"
-        "https://cuda-maintainers.cachix.org/"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
       auto-optimise-store = true;
       trusted-users = ["@wheel"];
