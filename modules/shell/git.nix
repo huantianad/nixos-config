dac3cdba3b6b7c476d0a0f4c308ed0a5d8560807
huantian <davidtianli@gmail.com>
2025-10-24 09:22:27 -0700

    homemanager.git: new options

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -19,17 +19,15 @@ in {
 
     home-manager.users.huantian.programs.git = {
       enable = true;
-      userName = "huantian";
-      userEmail = "davidtianli@gmail.com";
 
       signing.key = "~/.ssh/id_ed25519.pub";
       signing.signByDefault = true;
 
       lfs.enable = true;
 
-      diff-so-fancy.enable = true;
-
-      extraConfig = {
+      settings = {
+        user.name = "huantian";
+        user.email = "davidtianli@gmail.com";
         core.autocrlf = "input";
         core.askpass = mkIf config.modules.desktop.kde.enable (lib.getExe pkgs.kdePackages.ksshaskpass);
         gpg.format = "ssh";
@@ -40,6 +38,11 @@ in {
         rebase.autoSquash = "true";
         rebase.updateRefs = "true";
       };
+    };
+
+    home-manager.users.huantian.programs.diff-so-fancy = {
+      enable = true;
+      enableGitIntegration = true;
     };
   };
 }
