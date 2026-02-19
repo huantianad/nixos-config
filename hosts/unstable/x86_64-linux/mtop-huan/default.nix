3e1cac6180e73f1be7a85d58c54a522f4e7809ca
huantian <davidtianli@gmail.com>
2026-02-18 21:54:20 -0800

    mtop init

---
+++ hosts/unstable/x86_64-linux/mtop-huan/default.nix
@@ -0,0 +1,85 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    ../../../home.nix
+    ./hardware-configuration.nix
+  ];
+
+  system.stateVersion = "25.11";
+
+  modules = {
+    desktop = {
+      kde.enable = true;
+      random-apps.enable = true;
+      fonts.enable = true;
+      wayland.enable = true;
+
+      browsers = {
+        default = "librewolf";
+        librewolf.enable = true;
+        chromium.enable = true;
+      };
+
+      gaming = {
+        enable = true;
+        dolphin.enable = false;
+        dolphin.setUdevRules = true;
+        steam.enable = true;
+      };
+
+      programs = {
+        aw.enable = false;
+        fcitx.enable = true;
+        gamemode.enable = true;
+        kitty.enable = true;
+        qmk.enable = false;
+        tauon = {
+          enable = false;
+          openFirewall = true;
+        };
+        xbindkeys.enable = true;
+      };
+    };
+
+    dev = {
+      cc.enable = true;
+      tex.enable = false;
+      unity.enable = true;
+    };
+
+    editors = {
+      helix.enable = true;
+      vscode.enable = false;
+      vim.enable = false;
+      rider.enable = true;
+    };
+
+    hardware = {
+      bluetooth.enable = true;
+      nvidia.enable = false;
+      pipewire.enable = true;
+    };
+
+    services = {
+      usbmuxd.enable = false;
+    };
+
+    shell = {
+      zsh.enable = true;
+      direnv.enable = true;
+      doas.enable = true;
+      git.enable = true;
+      gnupg.enable = false;
+    };
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+}
