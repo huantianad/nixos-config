0791491118e01a458ddfaba3c418458d6f112fb4
huantian <davidtianli@gmail.com>
2026-02-20 20:01:59 -0800

    glfw3 fork no good

--- modules/desktop/gaming/default.nix
+++ modules/desktop/gaming/default.nix
@@ -19,14 +19,14 @@ in {
       cockatrice
       (prismlauncher.override {
         jdks = [jdk25];
-        glfw3-minecraft = glfw3.overrideAttrs (old: {
-          src = fetchFromGitHub {
-            owner = "LWJGL-CI";
-            repo = "GLFW";
-            rev = "c1636b906be78a9f59ad435297c6b9126e0cc10c";
-            hash = "sha256-WZWYCL3rP5OJxMDA+0747+XNvaEENTc5muG0qFZuE4c=";
-          };
-        });
+        # glfw3-minecraft = glfw3.overrideAttrs (old: {
+        #   src = fetchFromGitHub {
+        #     owner = "LWJGL-CI";
+        #     repo = "GLFW";
+        #     rev = "c1636b906be78a9f59ad435297c6b9126e0cc10c";
+        #     hash = "sha256-WZWYCL3rP5OJxMDA+0747+XNvaEENTc5muG0qFZuE4c=";
+        #   };
+        # });
       })
       # lutris
       # melonDS
